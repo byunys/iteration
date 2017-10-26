1f7b4483260a12a371e34818735e19f62306e979
byunys <33061713+byunys@users.noreply.github.com>
2017-10-26 01:04:48 -0600

    homework

--- iteration.py
+++ iteration.py
@@ -30,4 +30,27 @@ def congratulations(names, scores):
 		if (scores[i] == 100):
 			print "Congrats", names[i], "! You got a perfect score!"
 
+def sum(scores):
+	total = 0
+	for n in scores:
+		total += n
 
+	return total			
+
+def min(scores):
+	current_min = scores[0]
+	for n in scores:
+		if n < current_min:
+			current_min = n
+	return current_min
+
+
+def find_average(scores):
+	average = sum(scores)
+	for i in range(0, len(list)):
+		print average/i
+
+def find_filtered_average(scores):
+	average = sum(scores) - (current_min + current_min)
+	for i in range(0, len(list)):
+		print average/i
