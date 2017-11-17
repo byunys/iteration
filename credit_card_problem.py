9460fc23f4d9953a7820755ff9ed63be72f042bf
Yu Jin Sylvia Byun <byunys@s.dcsdk12.org>
2017-11-17 08:19:17 -0700

    incorporate divisibility rules

--- credit_card_problem.py
+++ credit_card_problem.py
@@ -1,8 +1,12 @@
 def count_digits(card_number):
+	digits = 0
+	end_digit = 0
 	if card_number/10 != 0:
-		digits = card_number%10
-		digits = card_number/10
-	return digits
+		card_number/10
+		digits += 1
+	else:
+		end_digit = 1
+	return digits + end_digit
 
 
-print count_digits(1234567891234567)
+print count_digits(5424180123456789)
