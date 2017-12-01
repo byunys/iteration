96faea54672a7afa45c82989c726fa98398fa745
Yu Jin Sylvia Byun <byunys@s.dcsdk12.org>
2017-12-01 08:18:29 -0700

    begin text_editor.py

---
+++ text_editor.py
@@ -0,0 +1,65 @@
+def replace_letter(original_letter, replacement_letter, word):
+	new_word = ""
+
+	for i in range(0, len(word)):
+		if word[i] == original_letter:
+			new_word += replacement_letter
+		else:
+			new_word += word[i]
+	return new_word
+
+def switch_letters(original_letter, replacement_letter, word):
+	new_word = ""
+
+	for i in range(0, len(word)):
+		if word[i] == original_letter:
+			new_word += replacement_letter
+		elif word[i] == replacement_letter:
+			new_word += original_letter
+		else:
+			new_word += word[i]
+	return new_word
+
+def switch_words(first_word, second_word, sentence):
+	new_sentence = ""
+	new_word = ""
+
+	for i in range(0, len(sentence)):
+		if sentence[i] == " ":
+			new_sentence += (new_word + " ")
+			new_word = ""
+		else:
+			new_word += sentence[i]
+			if new_word == first_word:
+				new_word = ""
+				new_sentence += second_word
+			elif new_word == second_word:
+				new_word = ""
+				new_sentence += first_word
+	return new_sentence
+
+def censor_text(sentence):
+	innapropriate_list = ["crappy", "dumb", "stupid"]
+	replacement_list = ["fuzzy", "smart", "cute"]
+	new_sentence = ""
+	new_word = ""
+
+	for i in range(0, len(sentence)):
+			if sentence[i] == " ":
+				new_sentence += (new_word + " ")
+				new_word = ""
+			else:
+				new_word += sentence[i]
+	for i in range(0, len(innapropriate_list)):			
+		if new_word == innapropriate_list[i]:
+			new_word = ""
+			new_sentence += replacement_list[i]
+	return new_sentence
+	#create for loop for lists too
+	#compare words to words and letter to letter
+
+
+print replace_letter("a", "l", "banana")
+print switch_letters("e", "o", "textbook")
+print switch_words("fox", "dog", "The quick brown fox jumps over the lazy dog")
+print censor_text("The dumb brown fox jumps over the stupid, crappy, lazy dog")
