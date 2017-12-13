d56bb73241d1a013dd2f002ef2051db17f7050a6
Yu Jin Sylvia Byun <byunys@s.dcsdk12.org>
2017-12-13 09:25:31 -0700

    start alphabetitizing

--- final.py
+++ final.py
@@ -10,27 +10,86 @@ emails = []
 genders = []
 ip_address = []
 
-
 for column in range(1, len(lines)):
 	info = lines[column].rstrip().split(",")
-	first_names.append(info[0])
-	last_names.append(info[1])
-	emails.append(info[2])
-	genders.append(info[3])
-	ip_address.append(info[4])
+	first_names.append(info[1])
+	last_names.append(info[2])
+	emails.append(info[3])
+	genders.append(info[4])
+	ip_address.append(info[5])
 
 
+#separate boys and girls
 def split_genders():
 	boys = []
 	girls = []
 
-	for gender in range(0, len(genders)):
-		if genders[gender] == "Male":
-			boys.append(first_names[gender])
-	return boys
+	for i in range(0, len(genders)):
+		if genders[i] == "Male":
+			boys.append(first_names[i] + " " + last_names[i])
+		elif genders[i] == "Female":
+			girls.append(first_names[i] + " " + last_names[i])
+	return [boys, girls]
+
+genders = split_genders()
 
 
-print split_genders()
+#alphabetitize
+def alphabetitize():
+	alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+	i = 0
+	j = 0
+	x = 0
+
+	for i in range(0, len(last_names)):
+		each_last_name = last_names[i]
+		new_last_name = ""
+
+		for j in range(0, len(each_last_name)):
+
+			for x in range(0, len(alphabet)):
+
+				if each_last_name[j] == alphabet[0]:
+					new_last_name += each_last_name[j]
+					j += 1
+					print new_last_name
+
+				else:
+					x += 1
+					i += 1
+					#if i > len(each_last_name):
+					#	i -=1
+
+	'''for i in range(0, len(last_names)):
+
+		each_last_name = last_names[i]
+		print each_last_name
+
+		for j in each_last_name:
+
+			if each_last_name[j] == "a":
+			
+				print "true"
+			
+			else:
+				j += 1'''
+
+
+
+'''
+print """Alphabetitize a list of names, splitting the boys and girls into two separate lists.
+"""
+
+print "boys: ", genders[0]
+print """
+girls: """, genders[1]
+'''
+print last_names, """
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+print alphabetitize()
+
 #def test_data():
 #	for i in range(0, len(names)):
 #		print 
@@ -38,10 +97,9 @@ print split_genders()
 
 #Alphabetitize names
 
-#Use each of these computing concepts:
 #variables
-#conditionals
-#functions
-#lists
-#iteration (cycling)
-#abstraction -> Read the answer to this Stack Overflow discussion for an explanation
+#conditionals         DONE
+#functions            DONE
+#lists                DONE
+#iteration (cycling)  DONE
+#abstraction
